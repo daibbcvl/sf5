163e45683a6ecfd73c640153eef4a73e5d724bc5
Phung Duong <phung.duong@nfq.asia>
2021-04-08 11:41:58 +0700

    wood template

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="default")
+     *
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->render('front/default/index.html.twig', [
+        ]);
+    }
+
+
+}
